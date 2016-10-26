4d37405ef06685cb10a85c42338e72b700d1d4b9
Eduardo Vioque <vioquedu@gmail.com>
2016-10-26 23:53:41 +0200

    webpack loader settings updated

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/config/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/config/settings.py
@@ -256,7 +256,7 @@ JINJA_TEMPLATES = Environment(
 WEBPACK_LOADER = {
     'DEFAULT': {
         'CACHE': not DEBUG,
-        'BUNDLE_DIR_NAME': 'static/bundle/', # must end with slash
+        'BUNDLE_DIR_NAME': 'bundle/', # must end with slash
         'STATS_FILE': os.path.join(BASE_DIR, 'frontend/webpack-stats.json'),
         'POLL_INTERVAL': 0.1,
         'IGNORE': ['.+\.hot-update.js', '.+\.map']
@@ -264,7 +264,7 @@ WEBPACK_LOADER = {
 }
 
 if not DEBUG:
-  WEBPACK_LOADER['DEFAULT']['BUNDLE_DIR_NAME'] = 'static/dist/'
+  WEBPACK_LOADER['DEFAULT']['BUNDLE_DIR_NAME'] = 'dist/'
   WEBPACK_LOADER['DEFAULT']['STATS_FILE'] = os.path.join(BASE_DIR, 'frontend/webpack-stats-prod.json'),
 
 
