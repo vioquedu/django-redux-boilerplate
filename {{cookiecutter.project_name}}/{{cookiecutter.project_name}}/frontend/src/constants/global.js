5ac6e8cbe6372a42c6cbb705888f40bf4220f9eb
Eduardo Vioque <vioquedu@gmail.com>
2016-10-27 01:43:54 +0200

    BASE_URL as a function of environment

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/frontend/src/constants/global.js
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/frontend/src/constants/global.js
@@ -1,3 +1,7 @@
 // Global constants
 
-export const BASE_URL = 'http://{{cookiecutter.domain_name}}:8000/'
+if (process.env.NODE_ENV === 'production') {
+  export const BASE_URL = 'http://{{cookiecutter.domain_name}}:8000/'
+} else {
+  export const BASE_URL = 'http://{{cookiecutter.domain_name}}:8000/'
+}
