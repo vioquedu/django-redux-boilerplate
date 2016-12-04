bdcb918a628aefffacaee55d54038919ae6d545b
vioquedu <vioquedu@gmail.com>
2016-12-04 16:19:36 +0100

    middleware updated

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/frontend/src/middleware/api.js
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/frontend/src/middleware/api.js
@@ -1,6 +1,29 @@
 import { Schema, arrayOf, normalize } from 'normalizr'
 import { camelizeKeys } from 'humps'
 import 'isomorphic-fetch'
+import {BASE_URL} from '../constants/global'
+
+function callApi(endpoint, authenticated) {
+  let token = localStorage.getItem('id_token') || null
+  let config = {}
+
+  if(authenticated) {
+    if(token){
+      config = {headers: {Authorization: "JWT " + `${token}`}}
+    } else {
+      throw "No token saved"
+    }
+  }
+
+  return fetch(BASE_URL + endpoint, config)
+    .then(response => response.text().then(text => ({text, response}))
+    ).then(({ text, response }) => {
+      if(!response.ok){
+	return Promise.rejext(text)
+      }
+      return JSON.parse(text)
+    }).catch(err => console.lot(err))
+}
 
 
 // Action key that carries API call info interpreted by this Redux middleware.
@@ -14,7 +37,7 @@ export default store => next => action => {
     return next(action)
   }
 
-  let { endpoint } = callAPI
+  let { endpoint, authenticated } = callAPI
   const { schema, types } = callAPI
 
   if (typeof endpoint === 'function') {
@@ -24,9 +47,9 @@ export default store => next => action => {
   if (typeof endpoint !== 'string') {
     throw new Error('Specify a string endpoint URL.')
   }
-  if (!schema) {
-    throw new Error('Specify one of the exported Schemas.')
-  }
+  /* if (!schema) {
+   *   throw new Error('Specify one of the exported Schemas.')
+   * }*/
   if (!Array.isArray(types) || types.length !== 3) {
     throw new Error('Expected an array of three action types.')
   }
@@ -43,7 +66,8 @@ export default store => next => action => {
   const [ requestType, successType, failureType ] = types
   next(actionWith({ type: requestType }))
 
-  return callApi(endpoint, schema).then(
+  //return callApi(endpoint, schema).then(
+  return callApi(endpoint, authenticated).then(
     response => next(actionWith({
       response,
       type: successType
