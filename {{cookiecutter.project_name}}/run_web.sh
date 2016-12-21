38fa4258cf7634833a8c25832d48b9c016a31880
Eduardo Vioque <vioquedu@gmail.com>
2016-12-21 01:21:54 +0100

    First release without node

--- {{cookiecutter.project_name}}/run_web.sh
+++ {{cookiecutter.project_name}}/run_web.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 # wait for PSQL server to start
-sleep 10
+sleep 1
 
-cd template_project
+cd {{cookiecutter.project_name}}
 # prepare init migration
 su -m manager -c "python manage.py makemigrations {{cookiecutter.project_name}}"
 # migrate db, so we have the latest db schema
 su -m manager -c "python manage.py migrate"
-# create super user
-su -m manager -c "python manage.py createsuperuser"
 # start development server on public ip interface, on port 8000
 su -m manager -c "python manage.py runserver 0.0.0.0:8000"
