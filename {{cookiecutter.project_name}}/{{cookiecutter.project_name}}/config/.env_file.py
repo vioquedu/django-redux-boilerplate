9e5442a0c6b9bf13780e6ad8f1b3e177db1f7c65
Eduardo Vioque <vioquedu@gmail.com>
2016-12-21 01:29:23 +0100

    Bug fixed in env file

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/config/.env_file.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/config/.env_file.py
@@ -4,4 +4,4 @@ ALLOWED_HOSTS=,
 DATABASE_URL=postgres://{{cookiecutter.db_user}}:{{cookiecutter.db_pwd}}@db/{{cookiecutter.db_name}}
 EMAIL_HOST_USER={{cookiecutter.author_email}}
 EMAIL_PWD=email_pwd 
-EMAIL_ADMIn={{cookiecutter.author_email}}
+EMAIL_ADMIN={{cookiecutter.author_email}}
